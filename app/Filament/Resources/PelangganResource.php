481278c5147bfb3e59f83397c39f0be5d8c6dd28
noffrizal <zaimjr@gmail.com>
2025-03-11 12:01:21 +0700

    laravel filament-pelanggan

---
+++ app/Filament/Resources/PelangganResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PelangganResource\Pages;
+use App\Filament\Resources\PelangganResource\RelationManagers;
+use App\Models\Pelanggan;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PelangganResource extends Resource
+{
+    protected static ?string $model = Pelanggan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('nama')->required(),
+                TextInput::make('no_hp'),
+                TextInput::make('alamat'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nama'),
+                TextColumn::make('no_hp'),
+                TextColumn::make('alamat'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPelanggans::route('/'),
+            'create' => Pages\CreatePelanggan::route('/create'),
+            'edit' => Pages\EditPelanggan::route('/{record}/edit'),
+        ];
+    }
+}
